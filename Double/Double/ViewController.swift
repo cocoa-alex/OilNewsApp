3eb56fdca9a5cdecfaa99a06cad1e05126d8480e
Steve Cheung <steve.cheung.1987@gmail.com>
2014-07-29 16:12:41 +0800

    add the files

---
+++ Double/Double/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Double
+//
+//  Created by steve on 14-7-15.
+//  Copyright (c) 2014年 steve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
