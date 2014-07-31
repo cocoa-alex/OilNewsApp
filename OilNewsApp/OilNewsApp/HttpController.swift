180f6bee9d513a37a4ed9e5655a3438721727d37
Steve Cheung <steve.cheung.1987@gmail.com>
2014-07-31 16:11:52 +0800

    test

---
+++ OilNewsApp/OilNewsApp/HttpController.swift
@@ -0,0 +1,15 @@
+//
+//  HttpController.swift
+//  OilNewsApp
+//
+//  Created by steve on 14-7-31.
+//  Copyright (c) 2014年 steve. All rights reserved.
+//
+
+import UIKit
+
+class HttpController:NSObject{
+
+    func onSearch(url:string){
+   }
+}
